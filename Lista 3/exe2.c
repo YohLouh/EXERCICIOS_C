d18f61b9f9afe21bf54ba87c04c342e631a0b38d
YohLouh <yohlourenco25@gmail.com>
2026-04-22 12:02:44 -0300

    Exercícios Lista 3

---
+++ Lista 3/exe2.c
@@ -0,0 +1,16 @@
+// Leia 5 números e mostre os pares
+#include <stdio.h>
+
+void main(){
+
+    int num[5];
+    for(int i = 0; i < 5; i++){
+        scanf("%d", &num[i]);
+    }
+    
+    for(int i = 0; i < 5; i++){
+        if (num[i] % 2 == 0){
+            printf("%d", num[i]);
+        }
+    }
+}
